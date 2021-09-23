c8f226eec4f44e3e4eec393a4373e1b720ac6bd9
prochini <a7771@gmail.com>
2021-09-24 07:33:17 +0800

    加入專案檔案。

---
+++ Test/Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+            var rng = new Random();
+                      
+
+            var option = new CookieOptions();
+            option.Expires = DateTime.Now.AddMonths(6);
+            option.Domain = "mobii";
+            option.Path = "/";
+            option.Expires = DateTime.Now.AddHours(3);
+            option.SameSite = SameSiteMode.None;
+            option.Secure = false;
+            Response.Cookies.Append("token", "123", option);
+            Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            Response.Redirect("http://localhost:46972/test");
+        }
+
+        [HttpOptions("/find")]
+        public IActionResult FindOptions()
+        {
+            Response.Headers.Add("Access-Control-Allow-Origin", new[] { (string)Request.Headers["Origin"] });
+            Response.Headers.Add("Access-Control-Allow-Headers", new[] { "Origin, X-Requested-With, Content-Type, Accept" });
+            Response.Headers.Add("Access-Control-Allow-Methods", new[] { "POST, OPTIONS" }); // new[] { "GET, POST, PUT, DELETE, OPTIONS" }
+            Response.Headers.Add("Access-Control-Allow-Credentials", new[] { "true" });
+            return NoContent();
+        }
+    }
+}
---
+++ Test/Controllers/testController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test.Controllers
+{
+    [ApiController]
+    [Microsoft.AspNetCore.Mvc.Route("[controller]")]
+    public class testController : ControllerBase
+    {
+        [HttpGet]
+        public int Get()
+        {
+            return 123;
+        }
+    }
+}
